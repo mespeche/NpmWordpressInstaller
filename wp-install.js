4f6c53a079a7a995d79a4ef9b569194b7f10e01a
Michaël Espeche <michael.espeche@gmail.com>
2016-03-17 21:03:02 +0100

    Put vars in config.json

--- wp-install.js
+++ wp-install.js
@@ -1,88 +1,15 @@
-var prompt  = require('prompt'),
+var config  = require('./config.json'),
+    prompt  = require('prompt'),
     colors  = require('colors/safe'),
     mkdirp  = require('mkdirp'),
     WP      = require('wp-cli');
-
-var initVars = {
-  properties: {
-    directory: {
-      message: 'Where do you want to install your new Wordpress project ?',
-      required: true,
-      default: 'wordpress'
-    },
-    locale: {
-      message: 'Select which language you want to download',
-      require: true,
-      default: 'en_US'
-    }
-  }
-};
-
-var configVars = {
-  properties: {
-    dbname : {
-      message: 'Database',
-      required: true,
-      default: 'wordpress'
-    },
-    dbuser : {
-      message: 'User',
-      required: true,
-      default: 'root'
-    },
-    dbpass : {
-      message: 'Password',
-      hidden: true,
-      replace: '*'
-    },
-    dbhost: {
-      message: 'Host',
-      required: true,
-      default: 'localhost'
-    },
-    dbprefix: {
-      message: 'Prefix'
-    },
-    dbcharset : {
-      message: 'Charset',
-      default: 'utf8'
-    }
-  }
-};
-
-var installVars = {
-  properties : {
-    url : {
-      message: 'Your website url',
-      required: true
-    },
-    title : {
-      message: 'Your website title',
-      required: true
-    },
-    admin_user : {
-      message: 'Your admin username',
-      required: true
-    },
-    admin_password : {
-      message: 'Your admin password',
-      required: true,
-      hidden: true,
-      replace: '*'
-    },
-    admin_email : {
-      message: 'Your admin email address',
-      required: true
-    }
-  }
-};
 
 welcome();
 
 prompt.start();
 prompt.message = '';
 
-prompt.get(initVars, function (err, result) {
+prompt.get(config.initVars, function (err, result) {
   if (err) {
     error(err);
   } else {
@@ -115,7 +42,7 @@ function wpDownload(WP, options) {
       succes(info);
 
       console.log(colors.cyan('Now lets set up your Wordpress environment.'));
-      prompt.get(configVars, function(err, result){
+      prompt.get(config.configVars, function(err, result){
         wpConfig(WP, result);
       });
     }
@@ -142,7 +69,7 @@ function wpDbCreate(WP) {
       succes(info);
 
       console.log(colors.cyan('Last step, create your admin account.'));
-      prompt.get(installVars, function(err, result){
+      prompt.get(config.installVars, function(err, result){
         wpInstall(WP, result);
       });
     }
